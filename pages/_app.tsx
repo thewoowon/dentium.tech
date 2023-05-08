f25ca46953ff6bfd204653987c37dd20b6affb52
thewoowon <thewoowon@naver.com>
2023-05-09 07:30:06 +0900

    refactor: 지침 내용 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,16 @@
 import { GoogleAnalytics } from '@/components/analytics/GoogleAnalytics';
 import { NaverAnalytics } from '@/components/analytics/NaverAnalytics';
-import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
 import React from 'react';
 import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
-    <ThemeProvider attribute="class" enableSystem={false} defaultTheme="light">
-      <GoogleAnalytics/>
-      <NaverAnalytics/>
+    <>
+      <GoogleAnalytics />
+      <NaverAnalytics />
       <Component {...pageProps} />
-    </ThemeProvider>
+    </>
   );
 };
 
