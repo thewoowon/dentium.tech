f6de47c4051ff85a9e5dad8a60132cce918a5f13
thewoowon <thewoowon@naver.com>
2023-05-10 21:45:58 +0900

    feat: 배너 구현 (추후 리팩토링 필요)

---
+++ constants/banner.ts
@@ -0,0 +1,17 @@
+import Banner from "@/components/Banner";
+
+export const bannerList:Banner[] = [
+    {
+        id: 1,
+        title: 'DENTECH',
+        image: 'https://picsum.photos/seed/picsum/1280/500',
+        subtitle: '우리가 만드는 문화, 그리고 기술'
+    },
+    {
+        id: 2,
+        title: 'Banner 2',
+        image: 'https://picsum.photos/seed/picsum/1280/500',
+        subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl sed aliquam ultricies, nunc nisl aliquet urna, vit'
+    },
+
+]
