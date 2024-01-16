bd4c1d70aa6d4cd85067bff05284fd9402ba3e3b
thewoowon <thewoowon@naver.com>
2024-01-17 07:45:21 +0900

    feat: add notice header

--- components/Notice.tsx
+++ components/Notice.tsx
@@ -3,6 +3,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import styled from '@emotion/styled';
+import Link from 'next/link';
 
 const Notice = () => {
   return (
@@ -12,7 +13,7 @@ const Notice = () => {
         loop={true}
         navigation={true}
         autoplay={{
-          delay: 3000,
+          delay: 5000,
           disableOnInteraction: false,
         }}
         modules={[Navigation, Autoplay]}
@@ -22,20 +23,26 @@ const Notice = () => {
         centeredSlides={true}
       >
         <SwiperSlide className="noticeSwiperSlide">
+          데보션에서도 덴테크를 만나보세요!
+          <Link
+            href="https://devocean.sk.com/"
+            target="_blank"
+            style={{
+              paddingLeft: '5px',
+            }}
+          >
+            SK 텔레콤 - 데보션
+          </Link>
+        </SwiperSlide>
+        <SwiperSlide className="noticeSwiperSlide">
+          새로운 모습으로 돌아올 덴데크를 기다려주세요! 🤗 ~2024.02
+        </SwiperSlide>
+        <SwiperSlide className="noticeSwiperSlide">
           GPT 요약 기능이 추가되었어요! 🤩 이제 더욱 편하게 글을 이해할 수
           있어요!
         </SwiperSlide>
         <SwiperSlide className="noticeSwiperSlide">
           새로운 글이 작성되었어요! 만나러 가볼까요? 😆
-        </SwiperSlide>
-        <SwiperSlide className="noticeSwiperSlide">
-          덴테크의 새로운 소식을 구독으로 받아보세요! ✉️
-        </SwiperSlide>
-        <SwiperSlide className="noticeSwiperSlide">
-          오늘의 커피챗은 무엇일까요? ☕️
-        </SwiperSlide>
-        <SwiperSlide className="noticeSwiperSlide">
-          덴테크가 새롭게 단장했어요! ✨
         </SwiperSlide>
       </Swiper>
     </NoticeContainer>
